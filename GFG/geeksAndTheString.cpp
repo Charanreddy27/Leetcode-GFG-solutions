d8bdfd78432faa9bc7ec9e3790b72c2fdf01015a
Charan <charanbapireddy@gmail.com>
2023-06-22 08:26:00 +0530

    geeks-and-the-string

---
+++ GFG/geeksAndTheString.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> inOrder(Node* root)
+    {
+        //code here
+        stack<Node*> st;
+        Node* t = root;
+        vector<int> v;
+        while(!st.empty() || t!=NULL){
+            if(t!=NULL){
+                st.push(t);
+                t=t->left;
+            }
+            else{
+                t = st.top();
+                v.push_back(t->data);
+                st.pop();
+                t=t->right;
+            }
+        }
+        return v;
+    }
+};
