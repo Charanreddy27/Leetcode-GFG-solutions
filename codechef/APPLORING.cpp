2245dfccea527a6103ec24da9cd6eceb94f5b340
Charan <charanbapireddy@gmail.com>
2023-07-07 08:10:18 +0530

    APPLORING

---
+++ codechef/APPLORING.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	    int n;
+	    cin>>n;
+	    int a,o;
+	    cin>>a>>o;
+	    if(a+o<=n){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	
+	return 0;
+}
