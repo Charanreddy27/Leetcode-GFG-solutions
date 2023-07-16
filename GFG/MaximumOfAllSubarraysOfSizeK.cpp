4eae85f94824f0016e23e161bf5a705bd9cc5690
Charan <charanbapireddy@gmail.com>
2023-07-16 10:06:34 +0530

    maximum-of-all-subarrays-of-size-k

---
+++ GFG/MaximumOfAllSubarraysOfSizeK.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+        vector<int> v;
+        multiset<int,greater<int>> m;
+        for(int i=0;i<k;i++){
+            m.insert(arr[i]);
+        }
+        v.push_back(*m.begin());
+        int i=0;int j=k;
+        while(j<n){
+            m.erase(m.find(arr[i]));
+            m.insert(arr[j]);
+            v.push_back(*m.begin());
+            i++;
+            j++;
+        }
+        return v;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i = 0;i<n;i++) 
+	        cin >> arr[i];
+	    Solution ob;
+	    vector <int> res = ob.max_of_subarrays(arr, n, k);
+	    for (int i = 0; i < res.size (); i++) 
+	        cout << res[i] << " ";
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
