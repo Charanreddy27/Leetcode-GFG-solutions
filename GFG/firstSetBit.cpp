3d913691527c633b03918328340f46c022dd5bae
Charan <charanbapireddy@gmail.com>
2023-07-19 22:22:30 +0530

    first-set-bit

---
+++ GFG/firstSetBit.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        // Your code here
+        vector<int> v;
+        while(n>0){
+            int rem = n%2;
+            v.push_back(rem);
+            n=n/2;
+        }
+        for(int i=0;i<v.size();i++){
+            if(v[i]==1) return i+1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
