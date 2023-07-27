b92f765148c6ddcc149d07d8910a0429fef58cf2
Charan <charanbapireddy@gmail.com>
2023-07-27 10:50:24 +0530

    remove duplicate in a string

---
+++ GFG/removeDuplicates.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    // code here
+	    set<char> h;
+	    string ans ="";
+	    for(int i=0;str[i]!='\0';i++){
+	        if(h.find(str[i])==h.end()){
+	            ans+= str[i];
+	            h.insert(str[i]);
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
