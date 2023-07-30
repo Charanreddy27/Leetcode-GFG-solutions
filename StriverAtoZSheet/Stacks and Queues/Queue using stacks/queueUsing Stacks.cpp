88728d5b19be2628c8fd9f385d6f1f150ee999d1
Charan <charanbapireddy@gmail.com>
2023-07-30 11:46:46 +0530

    striver-queue using stack

---
+++ StriverAtoZSheet/Stacks and Queues/Queue using stacks/queueUsing Stacks.cpp
@@ -0,0 +1,44 @@
+class MyQueue {
+public:
+    stack<int> input,output;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        while(!input.empty()){
+            output.push(input.top());
+            input.pop();
+        }
+        input.push(x);
+        while(!output.empty()){
+            input.push(output.top());
+            output.pop();
+        }
+    }
+    
+    int pop() {
+        int n = input.top();
+        input.pop();
+
+        return n;
+    }
+    
+    int peek() {
+        return input.top();
+    }
+    
+    bool empty() {
+        return input.empty();
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
