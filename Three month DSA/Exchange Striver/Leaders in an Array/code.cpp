9d718e6679495de276db63a57756bfeb8f53b8d5
Charan <charanbapireddy@gmail.com>
2023-09-20 20:08:03 +0530

    set matrix zeros"

---
+++ Three month DSA/Exchange Striver/Leaders in an Array/code.cpp
@@ -0,0 +1,21 @@
+vector<int> superiorElements(vector<int>&arr) {
+    // Write your code here.
+    vector<int> ans;
+    int n = arr.size();
+  
+ // Last element of an array is always a leader,
+ // push into ans array.
+ int max = arr[n - 1];
+ ans.push_back(arr[n-1]);
+
+  // Start checking from the end whether a number is greater
+  // than max no. from right, hence leader.
+  for (int i = n - 2; i >= 0; i--)
+    if (arr[i] > max) {
+      ans.push_back(arr[i]);
+      max = arr[i];
+    }
+
+  
+  return ans;
+}
