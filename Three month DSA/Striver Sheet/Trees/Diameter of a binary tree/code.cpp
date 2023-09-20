cdefef7b02cad798de44e9b628baf92c476adcfa
Charan <charanbapireddy@gmail.com>
2023-09-20 20:17:20 +0530

    diameter of a binary tree

---
+++ Three month DSA/Striver Sheet/Trees/Diameter of a binary tree/code.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameter(TreeNode* root , int &d){
+         if(root ==NULL) return 0;
+        int lh = diameter(root->left,d);
+        int rh = diameter(root->right,d);
+        d = max(d, lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int d =0;
+        diameter(root,d);
+        return d;
+    }
+};
