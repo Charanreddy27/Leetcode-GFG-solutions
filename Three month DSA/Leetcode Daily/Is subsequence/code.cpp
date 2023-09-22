ec436f3b184e2aff6ce69069dfb89676bc602731
Charan <charanbapireddy@gmail.com>
2023-09-22 08:36:31 +0530

    Is subsequence

---
+++ Three month DSA/Leetcode Daily/Is subsequence/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j =0;
+        while (i<s.size() and j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        if(i==s.size()) return true;
+        return false;
+    }
+};
