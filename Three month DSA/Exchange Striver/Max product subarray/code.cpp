47be1056851759a9dbe862c9f746539f3fdbe208
Charan <charanbapireddy@gmail.com>
2023-09-24 18:13:13 +0530

    max product subarray

---
+++ Three month DSA/Exchange Striver/Max product subarray/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre =1;
+        int suf =1;
+        int n = nums.size();
+        int ans = INT_MIN;
+        for(int i=0;i<n;i++){
+            if(pre ==0) pre=1;
+            if(suf==0) suf=1;
+            pre*= nums[i];
+            suf *= nums[n-i-1];
+            ans = max(ans,max(pre,suf));
+        }
+        return ans;
+    }
+};
