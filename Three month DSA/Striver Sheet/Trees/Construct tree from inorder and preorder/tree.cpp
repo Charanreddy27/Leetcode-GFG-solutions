791c0376ddad4ea91936871be33644a85695b3f6
Charan <charanbapireddy@gmail.com>
2023-09-24 06:36:50 +0530

    construct tree form inorder and preorder

---
+++ Three month DSA/Striver Sheet/Trees/Construct tree from inorder and preorder/tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* build(vector<int> &preorder ,int preStart,int preEnd,vector<int> &inorder,int inStart,int inEnd,map<int,int> &m){
+        if(preStart>preEnd || inStart >inEnd) return NULL;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inRoot = m[root->val];
+        int left = inRoot-inStart;
+        root->left = build(preorder,preStart+1,preStart+left,inorder,inStart,inRoot-1,m);
+        root->right = build(preorder,preStart+left+1,preEnd,inorder,inRoot+1,inEnd,m);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int,int> m;
+        for(int i=0;i<inorder.size();i++){
+            m[inorder[i]] =i;
+        }
+        TreeNode* root = build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,m);
+        return root;
+    }
+};
