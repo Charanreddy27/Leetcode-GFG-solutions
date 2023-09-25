4adf4e9f02936db6d7cfac6c2ff8de10a2bbd7b4
Charan <charanbapireddy@gmail.com>
2023-09-25 08:34:21 +0530

    BST iterator

---
+++ Three month DSA/Striver Sheet/Binary Search Trees/BST iterator/iterator.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* t = st.top();
+        st.pop();
+        pushAll(t->right);
+        return t->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+    void pushAll(TreeNode* root){
+        while(root){
+            st.push(root);
+            root=root->left;
+        }
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
