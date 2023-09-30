c0f4649c3e8409187b77a355199bac6da14c5076
Charan <charanbapireddy@gmail.com>
2023-09-30 20:18:38 +0530

    max alternate subsequece tabulation

---
+++ Three month DSA/Striver Sheet/DP/Max alternate subseqence/tabulation.cpp
@@ -0,0 +1,12 @@
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int notTake = dp[i-1];
+            dp[i]= max(take,notTake);
+        }
+        return dp[n-1];
+    }
