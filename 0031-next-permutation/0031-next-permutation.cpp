c2f6a94739ff1da75fbe4d1b48f5a51d8b9708c4
Marreddy Charan Bapi Reddy <charanbapireddy@gmail.com>
2024-06-07 22:13:55 +0530

    Time: 5 ms (20.41%), Space: 14.6 MB (14.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // next_permutation(nums.begin(),nums.end());
+        int idx = -1;
+        int n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx =i;
+                break;
+            }
+        }
+        if(idx==-1) return reverse(nums.begin(),nums.end());
+        for(int i=n-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx+1,nums.end());
     }
 };
