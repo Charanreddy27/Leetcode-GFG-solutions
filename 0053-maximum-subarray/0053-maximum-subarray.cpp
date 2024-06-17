6ffaf11eaf638c42a4a78e46fd61babfb3b815af
Marreddy Charan Bapi Reddy <charanbapireddy@gmail.com>
2024-06-17 22:13:43 +0530

    Time: 80 ms (42.60%), Space: 70.1 MB (67.18%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,10 +4,11 @@ public:
         int maxi = nums[0];
         int sum =nums[0];
         for(int i=1;i<nums.size();i++){
-            if(sum <0) sum=0;
+            if(sum<0) sum=0;
             sum+=nums[i];
             maxi = max(maxi,sum);
         }
         return maxi;
     }
+    
 };
